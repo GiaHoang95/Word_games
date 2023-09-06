916db333fb26bf8b8359178249b5b91cccbc67ba
GiaHoang95 <141647929+GiaHoang95@users.noreply.github.com>
2023-09-06 20:38:10 +0700

    Update Sanofi_Word_game.py

--- Sanofi_Word_game.py
+++ Sanofi_Word_game.py
@@ -18,10 +18,7 @@ div[class*="stTextInput"] label p {
 }
 
 [class="css-fg4pbf] {
-    background-color: #e5e5f7;
-    opacity: 0.8;
-    background-image: linear-gradient(0deg, #e5e5f7 50%, #444cf7 50%);
-    background-size: 10px 10px;
+    background: #e5e5f7;
 }
 
 </style>
