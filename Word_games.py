adbe13f76c4f818c26187bc08eb359d04460b4c7
GiaHoang95 <141647929+GiaHoang95@users.noreply.github.com>
2023-09-07 16:22:54 +0700

    Update Word_games.py

--- Word_games.py
+++ Word_games.py
@@ -8,4 +8,7 @@ co_ans2 = 'CONGRATS'
 co_ans3 = 'FANTASTICS'
 co_ans4 = 'LUCKY'
 
-col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15 =st.columns(15)
+col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15 = st.columns(15)
+
+with col1:
+  st.write('a')
