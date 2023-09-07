423072788cf51e9296fc5c83c6c8a9e639665470
GiaHoang95 <141647929+GiaHoang95@users.noreply.github.com>
2023-09-07 22:15:24 +0700

    Update Sanofi_Word_game.py

--- Sanofi_Word_game.py
+++ Sanofi_Word_game.py
@@ -721,8 +721,8 @@ submit_form = """
 <form action="https://formsubmit.co/giahoangduy@gmail.com" method="POST" enctype="multipart/form-data">
     <input type="text" placeholder="Key message" name="Key message" style="width: 100%;padding: 12px;border: 1px solid #ccc;border-radius: 6px; box-sizing: border-box;margin-top: 18px; margin-bottom: 16px;resize: vertical" required>
     <input type="text" placeholder="District Manager's name" name="District Manager's name" style="width: 100%;padding: 12px;border: 1px solid #cccccc;border-radius: 6px; box-sizing: border-box;margin-top: 6px; margin-bottom: 16px;resize: vertical" required>
-    <button type="submit" style="background-color: #04AA6D;color: white;padding: 12px 20px;border: none;border-radius: 6px;cursor: pointer;">Send</button>
     <input type="file" name="attachment" accept="image/png, image/jpeg">
+    <button type="submit" style="background-color: #04AA6D;color: white;padding: 12px 20px;border: none;border-radius: 6px;cursor: pointer;">Send</button>
 </form>
 """
 st.markdown(submit_form, unsafe_allow_html=True)
