4e91c042e33f846cbedf63b38414baab21f8eeb2
GiaHoang95 <141647929+GiaHoang95@users.noreply.github.com>
2023-09-08 10:03:38 +0700

    Update Sanofi_Word_game.py

--- Sanofi_Word_game.py
+++ Sanofi_Word_game.py
@@ -9,9 +9,9 @@ div[class*="stTextInput"] label p {
 }
 
 [data-testid="column"] {
-    width: calc((100/16-1,5)%-1,5rem) !important;
-    flex: 1 1 calc((100/16)%-1,5rem) !important;
-    min-width: calc((100/16)%-1,5rem) !important;
+    width: calc((100/16-1,5)%-2rem) !important;
+    flex: 1 1 calc((100/16)%-2rem) !important;
+    min-width: calc((100/16)%-2rem) !important;
 }
 
 [data-testid="stAppViewContainer"] {
